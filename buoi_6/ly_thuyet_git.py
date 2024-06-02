1a93103fd06307f06e76463bcbf595258c486f57
bupma <nguienlong2k3@gmail.com>
2024-06-02 13:49:45 +0700

    bai tap ly thuyet b6

--- buoi_6/ly_thuyet_git.py
+++ buoi_6/ly_thuyet_git.py
@@ -1,8 +1,23 @@
 print("---------HỌC GIT------------------")
 
+# Tạo file .git : Để lưu lịch sử/ kết nối với kho dữ liệu.
+# git init
 
-#Tạo file.git: để lưu lịch sử/ kết nối với kho dữ liệu
-#git init
+# Kết nối với kho dữ liệu git
+# git remote add origin https://github.com/nguienlong/hocpython1
 
-#kết nối với kho dữ liệu git
-#git remote add
+# kiểm tra có remote thành công không
+# git remote -v
+
+
+# Add vào kho lưu trữ tạm thời
+# git add .
+# Comment để đọc lại đ ã làm gì
+# git commit -v " comment"
+
+# Đẩy các commit len git
+# git push
+
+
+# Nếu push lần đầu tiên
+# git push --set-upstream origin master
